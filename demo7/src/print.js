ab064afc0c0c7973ad858b72d46071f594a362c6
yangkuo <734271234@qq.com>
2017-09-07 16:05:59 +0800

    demo78

--- demo7/src/print.js
+++ demo7/src/print.js
@@ -1,3 +1,5 @@
+import _ from 'lodash'
 export default function printMe() {
     console.log('I get called from print.js333338!');
+    console.log(_.join(['Another', 'module', 'loaded!'], ' '))
   }
--- demo7/webpack.prod.js
+++ demo7/webpack.prod.js
@@ -1,9 +1,13 @@
 const Merge = require('webpack-merge')
 const CommonConfig = require('./webpack.common.js')
 const UglifyJSPlugin  = require('uglifyjs-webpack-plugin')
-
+const webpack = require('webpack')
 module.exports = Merge(CommonConfig, {
+    devtool: 'cheap-module-source-map',
     plugins: [
-        new UglifyJSPlugin()
+        new UglifyJSPlugin(),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'common'  // 公共模块打包名称
+        })
     ]
 })
